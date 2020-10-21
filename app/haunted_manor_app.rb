ab5d5a5bbed2cb8f9a1b6888944aa3a7bc806cc5
leemichalee <leeisr@gmail.com>
2020-10-21 16:34:33 -0400

    updating fight

--- app/haunted_manor_app.rb
+++ app/haunted_manor_app.rb
@@ -103,7 +103,6 @@ class HauntedManorApp
             end
             if select == "New Game"
                 Player.start #wherever we want to start the game
-                Room.all.each {|room| room.monster_id = rand(1..8)}
             elsif select == "Update Account"
                 username = prompt.ask("Please choose a new Username.")
                 password = prompt.mask("Please choose a new Password.", mask: lantern)
--- app/models/room.rb
+++ app/models/room.rb
@@ -11,5 +11,39 @@ class Room < ActiveRecord::Base
         system("clear")
         puts current_room.description
         puts moonster
+        Room.fight
+    end
+
+    def self.fight
+        system("clear")
+        prompt = TTY::Prompt.new
+        select = prompt.select("What would you like to do?") do |option|
+            option.choice "Fight"
+            option.choice "Run"
+            option.choice "Stats"
+        end
+        if select == "Fight"
+            if @player.strength > Monster.find_by(id: current_room.monster_id).strength
+                Room.exit_check
+            elsif @player.strength < Monster.find_by(id: current_room.monster_id).strength
+                new_health = @player.health - 3
+                @player.update(health: new_health)
+            end
+        elsif select == "Run"
+        elsif select == "Stats"
+            system("clear")
+            @player.stats
+            #idk how to get the players stats here
+            select = prompt.select do |option|
+                option.choice "Go Back"
+            end
+            if select == "Go Back"
+                self.fight
+            end
+        end
+    end
+
+    def self.exit_check
+        system("clear")
     end
 end
