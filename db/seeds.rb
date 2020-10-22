00e55fa7f48642851e15ec2f6748fe70d98b30e8
nohaderf <freda.hon@gmail.com>
2020-10-22 11:44:13 -0400

    edited seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,7 @@ Monster.destroy_all
 Player.destroy_all
 Room.destroy_all
 
-# Monsters (name, strength)
-# monster1 = Monster.create(name: "Ghost", strength: 3)
-# monster2 = Monster.create(name: "Zombie", strength: 5)
-# monster3 = Monster.create(name: "Vampire", strength: 10)
-# MONSTERS IF WE CAN RANDOMIZE
+# Monsters (name, strength, number)
 monster1 = Monster.create(name: "Mummy", strength: 3, number: 1)
 monster2 = Monster.create(name: "Zombie", strength: 4, number: 2)
 monster3 = Monster.create(name: "Vampire", strength: 5, number: 3)
@@ -20,18 +16,7 @@ monster8 = Monster.create(name: "Ghost", strength: 3, number: 8)
 player1 = Player.create(username: "lee", password: "password", strength: 8, health: 10)
 player2 = Player.create(username: "freda", password: "anotherpassword", strength: 6, health: 8)
 
-# Rooms (name, description, monster_id, exit)
-# room1 = Room.create(name: "Kitchen", description: "You enter a tiny kitchen, where you notice all of the cabinet’s doors have been ripped off their hinges. Stains of every color painting the room, only visible from the faint light of the blue flames from the stove top. You hear rustling in the corner. The sounds of pots and pans falling onto each other, hitting the linoleum floor.  You turn your gaze to see a ghost!", monster_id: 1, exit: false)
-# room2 = Room.create(name: "Ballroom", description: "You enter a circular chamber, crystal chandeliers hanging, untouched and pristine as opposed to everything below. From shattered mirrors, to ravaged wallpaper, and chairs snapped like twigs all around. It all paled in comparison to being face to face with the ghost...", monster_id: 1, exit: false)
-# room3 = Room.create(name: "Conservatory", description: "The moonlight shines through the shattered glass of the conservatory, shedding light onto all the foliage retaking control of the land. Leaves and vines, splayed in and out of the once well-kept conservatory as if alive and breathing. You hear a rustling in the corner, and meeting your eyes is a VAMPIRE.", monster_id: 3, exit: false)
-# room4 = Room.create(name: "Dining Room", description: "Dust collected everywhere as far as the could see, spider webs wove loosely around books, dirtied shelves, and stands, The ground was littered with dirt, glass, books, and torn paper. The crevices in the wall allowed small amounts light to filter inside along with thin ropes of ivy.  Hearing shuffling, you turn to see a g-g-ghost!", monster_id: 1, exit: false)
-# room5 = Room.create(name: "Hall", description: "The ceiling must be twenty feet high. \nDesigns of fire and torture are carved into the moulding and small, demons glaring down at you from every angle. \nThere are 8 doors circling you.", exit: false)
-# room6 = Room.create(name: "Library", description: "Papers littered the floor circling the desk in the center of the room. Rats scurrying and slipping in the dimly lit room at the sound of your entrance. Cautiously approaching the desk with only a candle melting away you’re able to see that hiding behind the desk is a ZOMBIE!!!", monster_id: 2, exit: false)
-# room7 = Room.create(name: "Lounge", description: "The rough wear of paintwork and wooden splinters of the door meet you first. Squealing hinges announcing you to the room before you even step a foot in. The stench of booze and cigarettes assault your sense, where leisuring on the sofa you see zombie!", monster_id: 2, exit: false)
-# room8 = Room.create(name: "Billard Room", description: "Entering you immediately see the billiards table split savagely in half, with the foulest smelling stain coming from where both halves of the table cave into the floor. The cues are all sticking out of the walls as if they were darts. You hear the distinct sound of the start of a pool game and see a ghost!", monster_id: 1, exit: false)
-# room9 = Room.create(name: "Study", description: "The table dominated the room. An elongated ellipse of mahogany with brass at the edges. The chairs from the same tree. The table was set and ready for a feast, at least it was at some point in time before all this food became rotted. But the distinct sounds of eating without remorse can be heard, not stopping to meet your eyes is the vampire...", monster_id: 3, exit: false)
-
-# ROOMS IF WE CAN RANDOMIZE
+# Rooms (name, description)
 room1 = Room.create(name: "Kitchen", description: "You enter a tiny kitchen, where you notice all of the cabinet’s doors have been ripped off their hinges. \nStains of every color painting the room, only visible from the faint light of the blue flames from the stove top. \nYou hear rustling in the corner. \nThe sounds of pots and pans falling onto each other, hitting the linoleum floor.  \nYou turn your gaze to see a", monster_id: nil, exit: false)
 room2 = Room.create(name: "Ballroom", description: "You enter a circular chamber, crystal chandeliers hanging, untouched and pristine as opposed to everything below.  \nFrom shattered mirrors, to ravaged wallpaper, and chairs snapped like twigs all around.  \nIt all paled in comparison to being face to face with the", monster_id: nil, exit: true)
 room3 = Room.create(name: "Conservatory", description: "The moonlight shines through the shattered glass of the conservatory, shedding light onto all the foliage retaking control of the land.  \nLeaves and vines, splayed in and out of the once well-kept conservatory as if alive and breathing.  \nYou hear a rustling in the corner, and meeting your eyes is a", monster_id: nil, exit: false)
@@ -43,6 +28,6 @@ room8 = Room.create(name: "Billard Room", description: "Entering you immediately
 room9 = Room.create(name: "Study", description: "The table dominated the room.  \nAn elongated ellipse of mahogany with brass at the edges.  \nThe chairs from the same tree.  \nThe table was set and ready for a feast, at least it was at some point in time before all this food became rotted.  \nBut the distinct sounds of eating without remorse can be heard, not stopping to meet your eyes is the", monster_id: nil, exit: false)
 
 
-# Visited Rooms (player_id, room_id)
-vr_1 = VisitedRoom.create(player_id: player1.id, room_id: room1.id)
-vr_2 = VisitedRoom.create(player_id: player2.id, room_id: room1.id)
+# # Visited Rooms (player_id, room_id)
+# vr_1 = VisitedRoom.create(player_id: player1.id, room_id: room1.id)
+# vr_2 = VisitedRoom.create(player_id: player2.id, room_id: room1.id)
